91c36497981afdfe38df5899be7259b18b588792
Adsandro Carvalho <adsandroxerd@gmail.com>
2022-11-05 02:08:10 -0300

    Configurações do sonar

--- indexLineFollower/indexLineFollower.ino
+++ indexLineFollower/indexLineFollower.ino
@@ -1,3 +1,5 @@
+#include <hcsr04.h>
+
 #define DireitoPositivo 12 // signal pin 1 for the right motor, connect to IN1               
 #define DireitoNegativo 13  // signal pin 2 for the right motor, connect to IN2
 #define ControleDireito 11 // enable pin for the right motor (PWM enabled)
@@ -9,12 +11,20 @@
 #define SensorE 7 // Declara em qual porta estara saindo os dados dos sensores
 #define SensorD 6
 
+#define trigger 5
+#define echo 4
+
 bool sensorE = 0;
 bool sensorD = 0;
+int distancia = 0;
+
+HCSR04 ultrassonico(trigger, echo);
 
 
-int velocidade = 255; // Velocidade em condições perfeitas
-int erro = 145; // Velocidade em caso o carrinho saia da rota
+
+int velocidade = 150; // Velocidade em condições perfeitas
+int erro = 100; // Velocidade em caso o carrinho saia da rota
+int parado = 0;
 
 void setup() {
   
@@ -31,58 +41,52 @@ void setup() {
   pinMode(sensorE, INPUT);
   pinMode(sensorD, INPUT);  
 
-
+  delay(5000);
 }
 
 void loop(){
 
 sensorE = digitalRead(SensorE);
 sensorD = digitalRead(SensorD);
-Serial.println("");
-Serial.print("ValorE: ");
-Serial.print(sensorE);
-Serial.print("| ValorD: ");
-Serial.print(sensorD);
-  //Branco == 0, Preto == 1
-  //Manter sensores na linha branca  
-  //Frente
-  if ((sensorE == 0) and (sensorD == 0)){
+distancia = (ultrassonico.distanceInMillimeters());
+
+Serial.println(sensorD);
+Serial.println(sensorE);
+
+  if (distancia <= 60){
+      motorA(parado);
+      motorB(parado);
+    } 
+    
+   else if ((sensorE == 0) and (sensorD == 0)){
     motorA(velocidade);
     motorB(velocidade);
   }
 
-  //Direita
-  if ((sensorE == 1) and (sensorD == 0)){
-    motorA(velocidade);
-    motorB(erro);
+   else if ((sensorE == 1) and (sensorD == 0)){
+    motorA(erro);
+    motorB(velocidade);
   } 
 
-  //Esquerda 
-  if ((sensorE == 0) and (sensorD == 1)){
-   motorA(erro);
-   motorB(velocidade);   
+   else if ((sensorE == 0) and (sensorD == 1)){
+    motorA(velocidade);
+    motorB(erro);   
   }
 
-  // Ré
-  if ((sensorE == 1) and (sensorD == 1)){
-    motorB(0);
-    motorA(0);
+   else if ((sensorE == 0) and (sensorD == 0)){
+    motorA(velocidade);
+    motorB(velocidade);
   }  
-  
-//motorA(velocidade);
-//motorB(velocidade); 
   
 }
 
-//Motor Direita
-void motorA(int veloc){
+void motorB(int veloc){
 digitalWrite(DireitoPositivo,LOW);
 digitalWrite(DireitoNegativo,HIGH);
 analogWrite(ControleDireito,veloc);
 }
 
-//Motor Esquerda
-void motorB(int veloc){
+void motorA(int veloc){
 digitalWrite(EsquerdoPositivo,LOW);
 digitalWrite(EsquerdoNegativo,HIGH);
 analogWrite(ControleEsquerdo,veloc);
---
+++ sensorSonar/sensorSonar.ino
@@ -0,0 +1,21 @@
+#define echo 2
+#define trig 3
+unsigned long tempo;
+double distancia;
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+  digitalWrite(trig, LOW);
+}
+
+
+void loop() {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds (10);
+  digitalWrite(trig, LOW);
+  tempo = pulseIn(echo, HIGH);
+  distancia = tempo/58;
+  Serial.println(distancia);
+  delay(100);
+}
