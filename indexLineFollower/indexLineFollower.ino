0e7ca69ff8c5555ae86f30c42c3751ba9839be80
Adsandro Carvalho <adsandroxerd@gmail.com>
2022-11-13 19:54:50 -0300

    Adiciona novo sensor infravermelho (mudança de condições)

--- indexLineFollower/indexLineFollower.ino
+++ indexLineFollower/indexLineFollower.ino
@@ -8,38 +8,45 @@
 #define EsquerdoNegativo 8 // signal pin 2 for the left motor, connect to IN4
 #define ControleEsquerdo 10 // enable pin for the left motor (PWM enabled)
 
-#define SensorE 7 // Declara em qual porta estara saindo os dados dos sensores
-#define SensorD 6
+#define SensorE 5 // Declara em qual porta estara saindo os dados dos sensores
+#define SensorD 3
+#define SensorM 4
 
-#define trigger 5
-#define echo 4
+#define trigger 7
+#define echo 6
+
+#define led 2
 
 bool sensorE = 0;
 bool sensorD = 0;
-int distancia = 0;
+bool sensorM = 0;
+
+int distancia = 66;
 
 HCSR04 ultrassonico(trigger, echo);
 
 
-
 int velocidade = 150; // Velocidade em condições perfeitas
-int erro = 100; // Velocidade em caso o carrinho saia da rota
+int erro = 90; // Velocidade em caso o carrinho saia da rota
 int parado = 0;
 
 void setup() {
   
   Serial.begin(9600);
   
-  pinMode(DireitoPositivo,OUTPUT);        
-  pinMode(DireitoNegativo,OUTPUT);
-  pinMode(ControleDireito,OUTPUT);
+  pinMode(DireitoPositivo, OUTPUT);        
+  pinMode(DireitoNegativo, OUTPUT);
+  pinMode(ControleDireito, OUTPUT);
   
-  pinMode(EsquerdoPositivo,OUTPUT);        
-  pinMode(EsquerdoNegativo,OUTPUT);
-  pinMode(ControleEsquerdo,OUTPUT);
+  pinMode(EsquerdoPositivo, OUTPUT);        
+  pinMode(EsquerdoNegativo, OUTPUT);
+  pinMode(ControleEsquerdo, OUTPUT);
 
   pinMode(sensorE, INPUT);
   pinMode(sensorD, INPUT);  
+  pinMode(sensorM, INPUT);
+
+  pinMode(led, INPUT);
 
   delay(5000);
 }
@@ -48,35 +55,53 @@ void loop(){
 
 sensorE = digitalRead(SensorE);
 sensorD = digitalRead(SensorD);
+sensorM = digitalRead(SensorM);
 distancia = (ultrassonico.distanceInMillimeters());
 
-Serial.println(sensorD);
-Serial.println(sensorE);
 
-  if (distancia <= 60){
+Serial.println("");
+Serial.print("Distancia:");
+Serial.print(distancia);
+
+Serial.println("");
+Serial.print("Sensor Esquerdo:");
+Serial.print(sensorE);
+
+Serial.println("");
+Serial.print("Sensor Meio:");
+Serial.print(sensorM);
+
+Serial.println("");
+Serial.print("Sensor Direito:");
+Serial.print(sensorD);
+
+
+    if (distancia <= 100){
       motorA(parado);
       motorB(parado);
     } 
-    
-   else if ((sensorE == 0) and (sensorD == 0)){
-    motorA(velocidade);
-    motorB(velocidade);
+    else if ((sensorE == 0) and (sensorD == 0) and (sensorM == 0))  {
+      motorA(velocidade);
+      motorB(velocidade);
   }
-
-   else if ((sensorE == 1) and (sensorD == 0)){
-    motorA(erro);
-    motorB(velocidade);
+    else if ((sensorE == 0) and (sensorD == 1) and (sensorM == 0))  {
+      motorA(velocidade);
+      motorB(velocidade);
+  }
+    else if ((sensorE == 1) and (sensorD == 0) and (sensorM == 0))  {
+      motorA(velocidade);
+      motorB(velocidade);
+  }
+   else if ((sensorE == 1) and (sensorD == 0) and (sensorM == 1)) {
+      motorA(erro);
+      motorB(velocidade);
   } 
-
-   else if ((sensorE == 0) and (sensorD == 1)){
-    motorA(velocidade);
-    motorB(erro);   
+   else if ((sensorE == 0) and (sensorD == 1) and (sensorM == 1)){
+      motorA(velocidade);
+      motorB(erro);   
   }
 
-   else if ((sensorE == 0) and (sensorD == 0)){
-    motorA(velocidade);
-    motorB(velocidade);
-  }  
+  delay(0);
   
 }
 
@@ -91,4 +116,5 @@ digitalWrite(EsquerdoPositivo,LOW);
 digitalWrite(EsquerdoNegativo,HIGH);
 analogWrite(ControleEsquerdo,veloc);
 }
+
 
